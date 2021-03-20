6991865283fc939f5f20a2c7d28b565c7fe4852c
Travis Bumgarner <travisbumgarner@Traviss-MacBook-Pro.local>
2021-03-20 12:35:01 -0400

    Initial commit with meteor bootstrap

---
+++ tests/main.ts
@@ -0,0 +1,21 @@
+import { Meteor } from 'meteor/meteor';
+import assert from 'assert';
+
+describe('photonav3', function () {
+  it('package.json has correct name', async function () {
+    const { name } = await import('../package.json');
+    assert.strictEqual(name, 'photonav3');
+  });
+
+  if (Meteor.isClient) {
+    it('client is not server', function () {
+      assert.strictEqual(Meteor.isServer, false);
+    });
+  }
+
+  if (Meteor.isServer) {
+    it('server is not client', function () {
+      assert.strictEqual(Meteor.isClient, false);
+    });
+  }
+});
